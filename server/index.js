dbb8d13b6a954ad53923c931dce8b759c1d07c9d
Damien <djtouchette1993@gmail.com>
2017-01-25 06:52:37 -0700

    s

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get('*', (req, res) => {
 });
 
 server.listen(port, () => {
-//   console.log('listening on port ' + port);
+  console.log('listening on port ' + port);
 //   const post = new Post({
 //     content: "<p>This is my first blog post and I'm going to use it as a quick introduction. I'm web developer who specializes in API development, along with that I'm also a consultant. The main focus of these writings will be to document my learnings and tutorials on thing like React/Redux, Node and any other language or frameworks I can get my hands on.<\/p><p>Currently My main focus is to get this blog up and running, along with some adjustments to this site. If you have any suggestions, comment below! Assuming I got to putting the comment section in... In the meantime you can head over to <a href='https://damientouchette.io/contact'>to the contact page<\/a><\/p><p>Cheers<\/p>",
 //     heroImg: "https://i.ytimg.com/vi/PO26A3XOglE/maxresdefault.jpg",
