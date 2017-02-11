8f7b8727caed4eb54d9b55cbace1767d3936dd89
Damien <djtouchette1993@gmail.com>
2017-02-11 15:53:35 -0700

    Added touter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './containers/App/index';
 import './index.css';
 import routes from './routes';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, hashHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -21,7 +21,7 @@ const rootRoute = {
 ReactDOM.render(
   <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
     <Router
-      history={browserHistory}
+      history={hashHistory}
       routes={rootRoute}>
     </Router>
   </MuiThemeProvider>,
