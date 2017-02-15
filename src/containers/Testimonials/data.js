1dfa4db9de147a8709ba097a865ef700c7ac57c2
root <djtouchette1993@gmail.com>
2017-02-15 23:25:15 +0000

    Changed testimonial card

--- src/containers/Testimonials/data.js
+++ src/containers/Testimonials/data.js
@@ -2,12 +2,12 @@ import therelogo from '../../assets/images/therelogo.png';
 
 export const data = [
   {
-    img: therelogo,
+    img: 'http://www.twosocial.com/uploads/1434683751-emptyavatar.jpg',
     text: 'Damien was a great developer to work with. Very easy to talk to, and makes the best decisions for your situation. He was super responsive at all times, and his react and javascript skills and practices are very high. He also taught me a lot and showed me many new popular technologies we can use in our application. For example showing me Enzyme and Jest for react component testing and javascript unit testing!',
     title: 'Kareem',
   },
   {
-    img: therelogo,
+    img: 'http://www.twosocial.com/uploads/1434683751-emptyavatar.jpg',
     text: 'Ninja developer skills. I couldnt recommend more! Perfect in communication, really fast work!',
     title: 'Michael',
   },
