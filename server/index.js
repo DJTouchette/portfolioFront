79d31a16fc1eb972f8642f176fcbaf7ab92e63f1
Damien <djtouchette1993@gmail.com>
2017-02-21 14:38:39 -0700

    Added build location

--- server/index.js
+++ server/index.js
@@ -11,13 +11,14 @@ require('dotenv').config();
 applyMiddlewares(app);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
 // app.get('/about', (req, res) => {
 //   res.sendFile('build/index.html');
 // });
 server.listen(port, () => {
+  console.log(path.resolve(__dirname, '../build', 'index.html'));
   console.log('listening on port ' + port);
 //   const post = new Post({
 //     content: "<p>This is my first blog post and I'm going to use it as a quick introduction. I'm web developer who specializes in API development, along with that I'm also a consultant. The main focus of these writings will be to document my learnings and tutorials on thing like React/Redux, Node and any other language or frameworks I can get my hands on.<\/p><p>Currently My main focus is to get this blog up and running, along with some adjustments to this site. If you have any suggestions, comment below! Assuming I got to putting the comment section in... In the meantime you can head over to <a href='https://damientouchette.io/contact'>to the contact page<\/a><\/p><p>Cheers<\/p>",
