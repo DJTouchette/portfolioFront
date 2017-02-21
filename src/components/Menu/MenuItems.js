31552a838abfc401e5c40b41f0dd3149701a4fcc
Damien <djtouchette1993@gmail.com>
2017-02-21 14:03:43 -0700

    Added youtube to menu

--- src/components/Menu/MenuItems.js
+++ src/components/Menu/MenuItems.js
@@ -7,6 +7,17 @@ import './Menu.css';
 const MenuItems = (props) => {
   const { list, handleClick } = props;
   const items = list.map((item) => {
+    if (item.external)
+      return (
+        <MenuItem onTouchTap={handleClick.bind(this)} key={item.route}>
+          <a className="atag" target="_blank" href={item.route}>
+            <div className="icon">
+              <span>{item.text}</span>
+              <img className="youtube" alt={item.text} src={item.icon} />
+            </div>
+          </a>
+        </MenuItem>
+      )
     return (
       <MenuItem onTouchTap={handleClick.bind(this)} key={item.route}>
         <Link className="atag" to={item.route}>
--- src/components/Menu/constants.js
+++ src/components/Menu/constants.js
@@ -4,10 +4,12 @@ import projects from '../../assets/images/projects.svg';
 import home from '../../assets/images/home.svg';
 import contact from '../../assets/images/contact.svg';
 import blog from '../../assets/images/blog.svg';
+import youtube from '../../assets/images/youtubelogo.png';
 
 const menuItems = [
   { text: 'Home', icon: home, route: '/' },
   { text: 'Testimonials', icon: blog, route: '/testimonials' },
+  { external: true, text: 'Youtube Channel', icon: youtube, route: 'https://www.youtube.com/channel/UCzTvUZmrkCrbOKHTATeJITg' },
   { text: 'About Me', icon: about, route: '/about' },
   { text: 'Technologies', icon: technology, route: '/technologies' },
   { text: 'Projects', icon: projects, route: '/projects' },
